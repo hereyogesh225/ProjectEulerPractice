f96c9b91dcaa09a9938b29316c63ba41d7f625da
Yogesh <Yogesh.Here@west.com>
2020-03-22 23:01:36 +0530

    Done changes Again for sum upto 4 million

--- src/Even_Fibonaaci.java
+++ src/Even_Fibonaaci.java
@@ -7,19 +7,28 @@ public class Even_Fibonaaci {
 		
 		System.out.println("Enter a range upto which you want the series");
 		Scanner sc=new Scanner(System.in);
-		int a =sc.nextInt();
+		int a =4000000;
 		
 		int first=1;
 		int second=2;
-		System.out.print(" "+first+" "+second+" ");
+		//System.out.print(" "+first+" "+second+" ");
+		int sum=second;
 		int next=0;
-		while(next<a)
+		while(next<=a)
 		{
 			next=first+second;
-			System.out.print(next+" ");
+			//System.out.print(next+" ");
+			if(next%2==0)
+			{
+				sum=sum+next;
+			}
 			first=second;
 			second=next;
 		}
+		System.out.println("Sum of even numbers of fibonacci series is :"+sum);
 	}
 
+	
+	
+	
 }
